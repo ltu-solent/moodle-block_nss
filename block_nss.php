cc841fa4f010df28faa1710d02f7dc9341eede47
Sarah Cotton <sarah.cotton@solent.ac.uk>
2021-02-01 16:08:41 +0000

    Updated for 2021 survey
    Updated for 2021 survey
    
    
    update

--- block_nss.php
+++ block_nss.php
@@ -3,6 +3,10 @@ class block_nss extends block_base {
     public function init() {
         $this->title = get_string('nss', 'block_nss');
     }
+	
+	function has_config() {
+	  return true;
+	}
 
 	public function hide_header() {
 	  return true;
@@ -19,12 +23,10 @@ class block_nss extends block_base {
 		//Check date and which banner should be shown
 		$time = new DateTime("now", core_date::get_user_timezone_object());
 		$now = $time->getTimestamp();
-		$displaynss = 0;
 		
-		//if($now > 1579132800){ // TEST
-		if(($now > 1580083200) && ($now < 1588291199)){ // NSS 27/01/20 - 30/04/20				
+		if(($now > get_config('block_nss', 'displayfrom')) && ($now < get_config('block_nss', 'displayto'))){
 			// Display banner including tracking 
-			if($DB->record_exists_sql('SELECT nss FROM {nss} WHERE studentid = ?', array($USER->idnumber))){
+			if($DB->record_exists_sql('SELECT studentid FROM {nss} WHERE studentid = ?', array($USER->idnumber))){
 				$this->content->text = '<a href="http://www.thestudentsurvey.com/" target="_blank"  onClick="gtag(\'event\', \'click\', { \'event_category\': \'Survey Banner\', \'event_action\': \'Click\', \'event_label\': \'NSS\'});"><img src="/blocks/nss/images/nss.png" alt="NSS banner" class="img_nss"></a>';
 			}
 		}else{
--- lang/en/block_nss.php
+++ lang/en/block_nss.php
@@ -1,5 +1,7 @@
 <?php
-$string['pluginname'] = 'NSS/myCourse block';
+$string['pluginname'] = 'NSS/myCourse';
 $string['nss'] = 'NSS';
-$string['nss:addinstance'] = 'Add a new NSS/myCourse block';
+$string['nss:addinstance'] = 'Add a new NSS/myCourse';
 $string['nss:myaddinstance'] = 'Add a new NSS/myCourse block to the My Moodle page';
+$string['displayfrom'] = 'Unix timestamp of date to display the block from';
+$string['displayto'] = 'Unix timestamp of date to display the block until';
