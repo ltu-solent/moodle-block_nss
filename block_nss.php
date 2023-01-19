36ffce2719ff6df0b4887a3659dd5903a6d34cc9
Mark Sharp <mark.sharp@solent.ac.uk>
2023-01-19 09:55:56 +0000

    2023 NSS banner and text

--- block_nss.php
+++ block_nss.php
@@ -29,8 +29,8 @@ class block_nss extends block_base {
             if($DB->record_exists_sql('SELECT studentid FROM {nss} WHERE studentid = ?', array($USER->idnumber))){
                 $this->content->text = '<a href="https://www.thestudentsurvey.com/" target="_blank" ' .
                 'onClick="gtag(\'event\', \'click\', { \'event_category\': \'Survey Banner\', \'event_action\': \'Click\', \'event_label\': \'NSS\'});">' .
-                '<img src="/blocks/nss/images/nss.png" alt="Final year students: we want to know how you\'ve found your time at Solent - ' .
-                'complete the Nation Student Survey by Saturday 30th April and you could win a £300 Currys PC World voucher or one of ten £25 Amazon vouchers."' .
+                '<img src="/blocks/nss/images/nss.jpg" alt="Final year students: we want to know how you\'ve found your time at Solent. ' .
+                'Complete the National Student Survey by Sunday 30 April and you could win a £400 Currys PC World voucher or one of ten £50 Amazon vouchers."' .
                 ' class="img_nss"></a>';
             }
         }else{
